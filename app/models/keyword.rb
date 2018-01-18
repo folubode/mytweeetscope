e533df8ae6123265255e287936c9040c7f2baabe
Femi Olubode-Awosola <olucube@yahoo.co.uk>
2018-01-18 23:37:04 +1300

    pg gem...

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,15 +1,19 @@
 class Keyword < ApplicationRecord
 
-# def grab_twitts
-# 	client = Twitter::REST::Client.new do |config|
-# 	  config.consumer_key        = "56fr5L6D92AxgtItBMtobalrK"
-# 	  config.consumer_secret     = "rgnTSiI2JWAp65dXSFrSEFQdA8G3UNMp1SXkLKICMHTrPWDlYd"
-# 	  config.access_token        = "3271997323-M9HQq3roa2Byu2tg6bidew83SnFQZy7GJDs53d3"
-# 	  config.access_token_secret = "2RcBkCdZe98xg8L69BGZNO99AtfL2CbkWr4ig4maUejyh"
-# 	end
-# end
+def grab_twitts
+	client = Twitter::REST::Client.new do |config|
+	  config.consumer_key        = "56fr5L6D92AxgtItBMtobalrK"
+	  config.consumer_secret     = "rgnTSiI2JWAp65dXSFrSEFQdA8G3UNMp1SXkLKICMHTrPWDlYd"
+	  config.access_token        = "3271997323-M9HQq3roa2Byu2tg6bidew83SnFQZy7GJDs53d3"
+	  config.access_token_secret = "2RcBkCdZe98xg8L69BGZNO99AtfL2CbkWr4ig4maUejyh"
+	end
+end
 
-#  	client.search(self.keyword, :count => 3, result_type: "recent").take(3).collect do |tweet|
+ 	client.search(self.word, :count => 3, result_type: "recent").take(3).collect do |tweet|
+ 	  "#{tweet.user.screen_name}: #{tweet.text}"
+ 	end
+
+# client.search("heroku", :count => 3, result_type: "recent").take(3).collect do |tweet|
 #  	  "#{tweet.user.screen_name}: #{tweet.text}"
 #  	end
 
